8379b05e2d0443223798ead6e0fbd663e6946e66
huergoG <guadalupe.huergo@gmail.com>
2020-09-04 17:26:55 -0300

    Work in profress add schematic

--- my-custom-material-theme/src/my-custom-material-theme/index.ts
+++ my-custom-material-theme/src/my-custom-material-theme/index.ts
@@ -1,10 +1,15 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule,  externalSchematic, chain,  } from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function myCustomMaterialTheme(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function myCustomMaterialTheme(options: any): Rule {
+  // return async (host: Tree, _context: SchematicContext) => {
+  return async () => {
+    return chain([
+      externalSchematic('@angular/material', 'material-shell', {
+        project: options.project
+      })
+    ]);
   };
 }
